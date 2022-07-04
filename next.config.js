253d30a2e52ff28b37dee5f983ae122d1b93f9ff
Brs545 <baris.candogan@hotmail.com>
2022-07-04 18:43:02 +0300

    nitial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  images: {
+    domains: [
+      'upload.wikimedia.org',
+      'brandslogo.net',
+      'i.pinimg.com',
+      'encrypted-tbn0.gstatic.com',
+      'i.picsum.photos',
+      'picsum.photos',
+      'unsplash.com',
+      'nikovirtanen.com',
+      'i2.milimaj.com',
+    ],
+  },
+}
