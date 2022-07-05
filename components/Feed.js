2b01818ace36e539b8a1302f23c5cba5fd961238
Brs545 <baris.candogan@hotmail.com>
2022-07-05 23:59:37 +0300

    styles/Feed.module.css

--- components/Feed.js
+++ components/Feed.js
@@ -1,10 +1,26 @@
 import Stories from './Stories'
 import styles from '../styles/Feed.module.css'
-
+import Posts from './Posts'
+import poststyle from '../styles/Posts.module.css'
+import MiniProfile from './MiniProfile'
+import Suggestions from './Suggestions'
 function Feed() {
   return (
-    <div className={styles.grid}>
-      <Stories />
+    <div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div className={styles.stories}>
+          <Stories />
+        </div>
+        <div className={styles.profile}>
+          <MiniProfile />
+          <Suggestions />
+        </div>
+      </div>
+      <div className={poststyle.main}>
+        <div className={poststyle.posts}>
+          <Posts />
+        </div>
+      </div>
     </div>
   )
 }
