627a63479a8a123c6c7b90cc36623cb771a4e0fe
Brs545 <baris.candogan@hotmail.com>
2022-07-25 16:52:48 +0300

    sign in styling

--- components/Feed.js
+++ components/Feed.js
@@ -4,25 +4,66 @@ import Posts from './Posts'
 import poststyle from '../styles/Posts.module.css'
 import MiniProfile from './MiniProfile'
 import Suggestions from './Suggestions'
+import { useSession } from 'next-auth/react'
+import { userAgent } from 'next/server'
 function Feed() {
+  const { data: session } = useSession()
   return (
     <div>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <div className={styles.stories}>
-          <Stories />
-        </div>
+      {!session && (
+        <div className={styles.container}>
+          <div className={styles.flex}>
+            <div className={styles.stories}>
+              <Stories />
+            </div>
+            {session && (
+              <div className={styles.profile}>
+                <MiniProfile />
+                <div className={styles.suggestions}>
+                  <Suggestions />
+                </div>
+              </div>
+            )}
+          </div>
 
-        <div className={styles.profile}>
-          <MiniProfile />
-          <Suggestions />
+          <div className={poststyle.main}>
+            <div className={poststyle.posts}>
+              <Posts />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
+      {session && (
+        <div>
+          <div className={styles.flex}>
+            <div className={styles.stories}>
+              <div className={styles.userDiv}>
+                <img
+                  className={styles.userImg}
+                  src={session.user.image}
+                  alt=''
+                />
+                <p className={styles.userTitle}>{session.user.username}</p>
+              </div>
+              <Stories />
+            </div>
+            {session && (
+              <div className={styles.profile}>
+                <MiniProfile />
+                <div className={styles.suggestions}>
+                  <Suggestions />
+                </div>
+              </div>
+            )}
+          </div>
 
-      <div className={poststyle.main}>
-        <div className={poststyle.posts}>
-          <Posts />
+          <div className={poststyle.main}>
+            <div className={poststyle.posts}>
+              <Posts />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
