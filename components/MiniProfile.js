3c233987f3ebe4c30daeaaee6564dcfad418bd0b
Brs545 <baris.candogan@hotmail.com>
2022-07-25 16:55:19 +0300

    adding user info to mini profile

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,15 +1,23 @@
 import styles from '../styles/MiniProfile.module.css'
 import Image from 'next/image'
+import { colors } from '@material-ui/core'
+import { useSession } from 'next-auth/react'
 
 function MiniProfile() {
+  const { data: session } = useSession()
+  console.log(session)
   return (
-    <div className={styles.img}>
-      <Image
-        layout='fill'
-        src='https://i.pinimg.com/600x315/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.jpg'
-        alt=''
-        objectFit='cover'
-      />
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div className={styles.logodiv}>
+        <img className={styles.img} src={session?.user?.image} alt='' />
+      </div>
+      <div className={styles.welcome}>
+        <p style={{ fontWeight: 'bold' }}>{session?.user?.username}</p>
+        <p style={{ fontSize: '10px', color: 'rgb(110,110,110)' }}>
+          Welcome to instagram
+        </p>
+      </div>
+      <button className={styles.button}>Sign Out</button>
     </div>
   )
 }
