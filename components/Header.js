eb1cace1722c31379d25b39dad7e82fa3a03588e
Brs545 <baris.candogan@hotmail.com>
2022-07-27 11:57:50 +0300

    Sign in and modal changes

--- components/Header.js
+++ components/Header.js
@@ -9,9 +9,23 @@ import {
   MenuIcon,
   HomeIcon,
 } from '@heroicons/react/outline'
+import { signIn, useSession, signOut } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { useRecoilState } from 'recoil'
+import { modalstate } from '../atoms/modalAtoms'
+
 function Header() {
+  const { data: session } = useSession()
+  const router = useRouter()
+  const [open, setOpen] = useRecoilState(modalstate)
   return (
-    <div style={{ padding: 10, borderBottom: '1px solid whitesmoke' }}>
+    <div
+      style={{
+        padding: 10,
+        borderBottom: '2px solid whitesmoke',
+        background: 'white',
+      }}
+    >
       <div
         style={{
           maxWidth: 1000,
@@ -21,6 +35,7 @@ function Header() {
         <div className={styles.container}>
           <div className={styles.relative}>
             <Image
+              onClick={() => router.push('/')}
               src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png'
               layout='fill'
             />
@@ -32,27 +47,72 @@ function Header() {
             />
           </div>
 
-          <div className={styles.div}>
-            <div className={styles.icondiv}>
-              <SearchIcon className={styles.icon} />
-            </div>
-            <input className={styles.input} type='text' placeholder='Search' />
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <HomeIcon className={styles.home} />
-            <PaperAirplaneIcon className={styles.paper} />
-            <div className={styles.number}>3</div>
-            <PlusCircleIcon className={styles.circle} />
-            <MenuIcon className={styles.menu} />
-            <UserGroupIcon className={styles.group} />
-            <HeartIcon className={styles.heart} />
-            <div className={styles.logo2}>
-              <Image
-                src='https://i.pinimg.com/600x315/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.jpg'
-                layout='fill'
-              />
-            </div>
-          </div>
+          {session ? (
+            <>
+              <div className={styles.div}>
+                <div className={styles.icondiv}>
+                  <SearchIcon className={styles.icon} />
+                </div>
+
+                {!open && (
+                  <input
+                    className={styles.input}
+                    type='text'
+                    placeholder='Search'
+                  />
+                )}
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <HomeIcon className={styles.home} />
+                <PaperAirplaneIcon className={styles.paper} />
+                <div className={styles.number}>3</div>
+
+                <PlusCircleIcon
+                  onClick={() => setOpen(true)}
+                  className={styles.circle}
+                />
+                <MenuIcon className={styles.menu} />
+                <UserGroupIcon className={styles.group} />
+                <HeartIcon className={styles.heart} />
+                <div className={styles.logodiv}>
+                  {open && (
+                    <img
+                      className={styles.logo3}
+                      src={session.user.image}
+                      alt=''
+                    />
+                  )}
+                  {!open && (
+                    <img
+                      onClick={signOut}
+                      className={styles.logo2}
+                      src={session.user.image}
+                      alt=''
+                    />
+                  )}
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <HomeIcon
+                  onClick={() => router.push('/')}
+                  className={styles.home}
+                />
+                <button
+                  style={{
+                    backgroundColor: 'white',
+                    border: 'none',
+                    cursor: 'pointer',
+                  }}
+                  onClick={signIn}
+                >
+                  Sign In
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
