4188b1a9aeac6119f142d599906b39730ceb5d65
Brs545 <baris.candogan@hotmail.com>
2022-07-28 21:39:26 +0300

    making modal opacity for modal

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,23 +1,47 @@
 import styles from '../styles/MiniProfile.module.css'
-import Image from 'next/image'
-import { colors } from '@material-ui/core'
 import { useSession } from 'next-auth/react'
+import { signOut } from 'next-auth/react'
+import { useRecoilState } from 'recoil'
+import { modalstate } from '../atoms/modalAtoms'
+import { display } from '@mui/system'
 
 function MiniProfile() {
   const { data: session } = useSession()
+  const [open, setOpen] = useRecoilState(modalstate)
+
   console.log(session)
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <div className={styles.logodiv}>
-        <img className={styles.img} src={session?.user?.image} alt='' />
-      </div>
-      <div className={styles.welcome}>
-        <p style={{ fontWeight: 'bold' }}>{session?.user?.username}</p>
-        <p style={{ fontSize: '10px', color: 'rgb(110,110,110)' }}>
-          Welcome to instagram
-        </p>
-      </div>
-      <button className={styles.button}>Sign Out</button>
+    <div>
+      {open && (
+        <div style={{ opacity: 0.5, display: 'flex' }}>
+          <div className={styles.logodiv}>
+            <img className={styles.img} src={session?.user?.image} alt='' />
+          </div>
+          <div className={styles.welcome}>
+            <p style={{ fontWeight: 'bold' }}>{session?.user?.username}</p>
+            <p style={{ fontSize: '10px', color: 'rgb(110,110,110)' }}>
+              Welcome to instagram
+            </p>
+          </div>
+          <button className={styles.button1}>Sign Out</button>
+        </div>
+      )}
+      {!open && (
+        <div style={{ display: 'flex' }}>
+          <div className={styles.logodiv}>
+            <img className={styles.img} src={session?.user?.image} alt='' />
+          </div>
+          <div className={styles.welcome}>
+            <p style={{ fontWeight: 'bold' }}>{session?.user?.username}</p>
+            <p style={{ fontSize: '10px', color: 'rgb(110,110,110)' }}>
+              Welcome to instagram
+            </p>
+          </div>
+          <button onClick={signOut} className={styles.button}>
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   )
 }
